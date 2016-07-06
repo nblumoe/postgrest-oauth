fa09003be49acc50960685a7149ca51447569f7f
Nils Blum-Oeste <nils.blum-oeste@wooga.net>
2016-07-06 19:25:34 +0200

    Add basic middleware skeletion
    For now just wrapping the basic postgrest application
    without adding additional functionality from the middleware.

--- app/Main.hs
+++ app/Main.hs
@@ -2,5 +2,30 @@
 
 module Main where
 
+import PostgREST.Config (AppConfig (..),
+                         readOptions)
+import           PostgREST.DbStructure
+
+import PostgRESTOAuth
+
+import qualified Hasql.Pool                           as P
+import           Data.IORef
+import           Data.String.Conversions              (cs)
+
+import           Network.Wai.Handler.Warp
+
+
 main :: IO ()
-main = putStrLn "Not yet implemented"
+main = do
+  conf <- readOptions
+  let port = configPort conf
+      pgConfig = cs $ configDatabase conf
+      poolConfig = configPool conf
+      appSettings = setPort port
+        . setServerName  "postgrest"
+        $ defaultSettings
+  pool <- P.acquire (poolConfig, 10, pgConfig)
+  structure <- P.use pool $ getDbStructure (cs $ configSchema conf)
+  refDbStructure <- newIORef $ either (error.show) id structure
+  putStrLn $ "Listening on port " ++ show port
+  runSettings appSettings $ postgrestOAuthApp conf refDbStructure pool
--- src/PostgRESTOAuth.hs
+++ src/PostgRESTOAuth.hs
@@ -1,1 +1,18 @@
-module PostgRESTOAuth where
+module PostgRESTOAuth
+  ( postgrestOAuthApp
+  ) where
+
+import qualified Hasql.Pool as H
+
+import GHC.IORef
+import PostgREST.App as PGR
+import PostgREST.Config as PGR
+import PostgREST.Types as PGR
+import qualified Network.Wai                    as Wai
+
+postgrestOAuthApp :: PGR.AppConfig
+  -> IORef PGR.DbStructure
+  -> H.Pool
+  -> Wai.Application
+postgrestOAuthApp conf refDbStructure pool =
+  postgrest conf refDbStructure pool
